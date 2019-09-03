2e7932bfa3d2f1885fbb23bc6627d50f7843cda4
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-03 12:21:44 +1000

    ADD TEST TO BUS CLASS

---
+++ src/TestBus.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+class TestBus {
+
+
+
+    @Test
+    void testBus() {
+
+        Bus bus = new Bus(0, 2, "Bike", Color.green);
+    }
+}
