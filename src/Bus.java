22297edc2b5049a769cec5aca0f4490539731871
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-03 12:14:59 +1000

    Add Constructor to Bus and Car

--- src/Bus.java
+++ src/Bus.java
@@ -1,2 +1,9 @@
+import java.awt.*;
+
 public class Bus extends Vehicle {
+
+    public Bus(int positionAtX, int positionAtY, String name, Color color) {
+        super(positionAtX, positionAtY, name, color);
+    }
+
 }
--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,7 @@
+import java.awt.*;
+
 public class Car extends Vehicle {
+    public Car(int positionAtX, int positionAtY, String name, Color color) {
+        super(positionAtX, positionAtY, name, color);
+    }
 }
