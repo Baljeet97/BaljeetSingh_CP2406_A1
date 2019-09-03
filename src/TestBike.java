b26d3b665c467d2802bba3fef0be8e478125da54
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-03 12:19:49 +1000

    ADD TEST TO BIKE CLASS

---
+++ src/TestBike.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+class TestBike {
+
+    @Test
+    void bikeTest() {
+
+        Bike bike = new Bike(0, 2, "Bike", Color.BLUE);
+    }
+}
