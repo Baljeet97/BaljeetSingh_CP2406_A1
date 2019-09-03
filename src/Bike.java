d31880167b28179861a04a9597c134eafa644fe4
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-03 12:14:36 +1000

    Add Constructor to Bike

--- src/Bike.java
+++ src/Bike.java
@@ -1,2 +1,8 @@
+import java.awt.*;
+
 public class Bike extends Vehicle {
+
+    public Bike(int positionAtX, int positionAtY, String name, Color color) {
+        super(positionAtX, positionAtY, name, color);
+    }
 }
