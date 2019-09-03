ae8e20262400b64e507edd574332c19b852508f2
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-03 12:13:56 +1000

    Add Constructor to Vehicle.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,21 @@
-public class Vehicle {
+import java.awt.*;
+
+class Vehicle {
+
+    private int positionAtX;
+    private int getPositionAtY;
+    private String name;
+    private Color color;
+
+    public Vehicle(int positionAtX, int positionAtY, String name, Color color){
+        this.positionAtX = positionAtX;
+        this.getPositionAtY = positionAtY;
+        this.name = name;
+        this.color = color;
+
+
+    }
+
+
+
 }
