825338911a60f1c25ba74710e9fbaf9ae332eb54
Baljeet <baljeetsingh@my.jcu.edu.au>
2019-09-07 17:05:14 +1000

    ADD TrafficLight

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,6 @@
 import java.awt.*;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle {
 
 
     public Car(int startPointX, int startPointy, String name, Color color, int speed, int size) {
--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,4 +1,25 @@
-class TrafficLight{
+import java.util.Random;
 
+public class TrafficLight {
+
+    public TrafficLight(boolean isGreen, boolean isRed) {
+
+    }
+
+
+    static int trafficController() {
+        Random rand = new Random();
+        int lightStatus = rand.nextInt(2);
+
+        if (lightStatus == 0) {
+            System.out.println("light is red");
+
+        } else System.out.println("light is green");
+
+        return lightStatus;
+    }
 
 }
+
+
+
